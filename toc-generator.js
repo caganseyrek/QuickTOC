586630c7949ace876ad591902e18bd19cdcf0422
Çağan Seyrek <115536535+caganseyrek@users.noreply.github.com>
2024-03-10 17:49:00 +0300

    Added option to include h1 tag in generation

--- toc-generator.js
+++ toc-generator.js
@@ -1,16 +1,26 @@
 window.onload = function () {
+    var includeh1 = true;
+
     var pageContents = document.getElementById("page-contents");
-    var headings = pageContents.querySelectorAll("h2,h3,h4");
+    var headings = pageContents.querySelectorAll("h1,h2,h3,h4");
+
     var toclist = document.createElement("ol");
 
     var prevHeading = "";
     var prevSubList = 0;
     var prevDoubleSublist = 0;
+    var prevTripleSublist = 0;
 
     for (i = 0; i <= headings.length - 1; i++) {
         var id = headings[i].textContent.toLowerCase().replace(/ /g, "-");
         var level = headings[i].localName.replace("h", "");
         var text = headings[i].textContent;
+
+        if (includeh1 === false) {
+            var _temp = Number(level);
+            _temp--;
+            level = _temp.toString();
+        }
 
         headings[i].setAttribute("id", id);
         var listitem = document.createElement("li");
@@ -19,10 +29,10 @@ window.onload = function () {
         anchor.textContent = text;
         listitem.appendChild(anchor);
 
-        if (level == 2) {
+        if (level == 1) {
             toclist.appendChild(listitem);
             prevHeading = level;
-        } else if (level == 3) {
+        } else if (level == 2) {
             if (prevHeading == level) {
                 document.getElementsByClassName("toc-sublist")[prevSubList - 1].appendChild(listitem);
             } else {
@@ -33,7 +43,7 @@ window.onload = function () {
                 toclist.appendChild(sublist);
             }
             prevHeading = level;
-        } else if (level == 4) {
+        } else if (level == 3) {
             if (prevHeading == level) {
                 document.getElementsByClassName("toc-doublesublist")[prevDoubleSublist - 1].appendChild(listitem);
             } else {
@@ -42,6 +52,17 @@ window.onload = function () {
                 doubleSubList.setAttribute("class", "toc-doublesublist");
                 doubleSubList.appendChild(listitem);
                 document.getElementsByClassName("toc-sublist")[prevSubList - 1].appendChild(doubleSubList);
+            }
+            prevHeading = level;
+        } else if (level == 4) {
+            if (prevHeading == level) {
+                document.getElementsByClassName("toc-triplesublist")[prevTripleSublist - 1].appendChild(listitem);
+            } else {
+                var tripleSublist = document.createElement("ul");
+                prevTripleSublist++;
+                tripleSublist.setAttribute("class", "toc-triplesublist");
+                tripleSublist.appendChild(listitem);
+                document.getElementsByClassName("toc-doublesublist")[prevSubList - 1].appendChild(tripleSublist);
             }
             prevHeading = level;
         }
